f04c0c2ff8892a9e5f694e8681f6270cd398091f
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-09-30 20:21:22 +0200

    fix: If not exists should be before the table

--- dags/sql/staging/tables/create_table_npi_drug.sql
+++ dags/sql/staging/tables/create_table_npi_drug.sql
@@ -1,4 +1,4 @@
-CREATE TABLE fraud.NPI_DRUG IF NOT EXISTS (npi text,
+CREATE TABLE IF NOT EXISTS fraud.NPI_DRUG (npi text,
 nppes_provider_last_org_name text,
 nppes_provider_first_name text,
 nppes_provider_city text,
